34ff379ae77bf41de9982df42c840aaf9724afe2
Fabrizzio Rutigliano <fabrizzio.rutigliano@gdn.com.uy>
2025-11-24 23:42:45 -0300

    Base para trabajar archivos

--- Archivos.h
+++ Archivos.h
@@ -3,9 +3,12 @@
 #include "AlumnosTree.h"
 #include "RegistrosList.h"
 
+/*
+// ---------- CREAR --------------
 // Agrega el nuevo entero al final del archivo
 void crearArchivo(String nomArch);
 
+// ---------- CONSULTAR ------------
 // Determina si existe o no un archivo con el nombre recibido por parámetro
 Boolean existeArchivo(String nomArch);
 
@@ -15,14 +18,53 @@ Boolean esVacioArchivo(String nomArch);
 // Determina si el entero recibido está en el archivo. Precondición: El archivo existe
 Boolean perteneceArchivo(String nomArch, int entero);
 
-// Devuelve la cantidad de enteros almacenados en el archivo. Precondición: El archivo existe
-int largoArchivo(String nomArch);
+// ---------- AB STRING ------------
+// Escribe en el archivo los caracteres del string s (incluido '\0')
+// Precondición: El archivo viene abierto para escritura.
 
-// Devuelve el k-ésimo entero almacenado en el archivo. Precondición: El archivo existe
-// y su largo es  k.
-int KesimoArchivo(String nomArch, int k);
+void Bajar_String (String s, FILE * f);
 
-// Despliega por pantalla los enteros almacenados en el archivo. Precondición: El archivo existe
-void desplegarArchivo(String nomArch);
+// Lee desde el archivo los caracteres del string s.
+// Precondición: El archivo viene abierto para lectura.
+void Levantar_String (String &s, FILE * f);
 
+// ---------- AB STRUCT ------------
+
+// Escribe en el archivo los datos de la persona P.
+// Precondición: El archivo viene abierto para escritura.
+void Bajar_Persona (Persona P, FILE * f );
+
+// Lee desde el archivo los datos de la persona P.
+// Precondición: El archivo viene abierto para lectura.
+void Levantar_Persona (Persona &P, FILE * f );
+
+// ---------- AB LISTA ------------
+
+// Abre el archivo para escritura y escribe los datos de todas las
+// personas que están almacenadas en la lista
+void Bajar_Lista (Lista L, String nomArch);
+
+// Abre el archivo para lectura y carga en la lista los datos de
+// todas las personas que están almacenadas en el archivo (llamando
+// al procedimiento InsBack).
+void Levantar_Lista (Lista &L, String nomArch);
+
+// ---------- AB ABB ------------
+
+// Escribe en el archivo los datos de todos los enteros del árbol
+// en forma recursiva. // Precondición: El archivo viene abierto para escritura.
+void Bajar_ABB_Aux (ABB a, FILE * f);
+
+// Abre el archivo para escritura y escribe los datos de todos los
+// enteros del árbol (llamando al procedimiento anterior)
+void Bajar_ABB (ABB a, String nomArch);
+
+// Abre el archivo para lectura e inserta en el árbol todos los
+// enteros están en el archivo (llamando al procedimiento Insert).
+// Precondición: El archivo existe.
+void Levantar_ABB (ABB &a, String nomArch);
+
+
+*/
 #endif // ARCHIVOS_H_INCLUDED
+
